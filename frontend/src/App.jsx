31cfd75400f35c4c0c18aa35d2194c2385e1ecbf
RaiSajid50 <raisajid94@gmail.com>
2026-05-05 10:30:55 +0500

    user error resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignupPage, ActivationPage, Home,ProductsPage ,BestSellingPage,EventPage,FAQPage} from "./Routes.js";
+import {
+  LoginPage,
+  SignupPage,
+  ActivationPage,
+  Home,
+  ProductsPage,
+  BestSellingPage,
+  EventPage,
+  FAQPage,
+} from "./Routes.js";
+
 import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-import axios from "axios";
-import { server } from "./server.js";
+
+import { useDispatch } from "react-redux";
+import { loadUser } from "./redux/actions/user.action.js";
+
 
 export const App = () => {
- return (
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/activation/:token" element={<ActivationPage />} />
-        <Route path ="/products" element ={<ProductsPage/>}/>
-        <Route path ='/best-selling' element ={<BestSellingPage/>}/>
-        <Route path = '/events' element ={<EventPage/>}/>
-        <Route path="/faq" element ={<FAQPage/>}/>
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/best-selling" element={<BestSellingPage />} />
+        <Route path="/events" element={<EventPage />} />
+        <Route path="/faq" element={<FAQPage />} />
       </Routes>
 
       <ToastContainer
@@ -34,5 +51,5 @@ export const App = () => {
         transition={Bounce}
       />
     </BrowserRouter>
- )
+  );
 };
