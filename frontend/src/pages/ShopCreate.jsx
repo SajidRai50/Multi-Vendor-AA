eb5529979f2e2567e5ed74fbbd0f2bf3c2900bd2
RaiSajid50 <raisajid94@gmail.com>
2026-05-20 08:05:25 +0500

    protected route for ShopRegistration...done 2nd part

--- frontend/src/pages/ShopCreate.jsx
+++ frontend/src/pages/ShopCreate.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {ShopRegistration} from '../components/Shop/ShopRegistration.jsx'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 export const ShopCreate = () => {
+  const navigate = useNavigate();
+    const {isSeller,seller} = useSelector((state)=>state.seller);
+  useEffect(() => {
+    if (isSeller) {
+      navigate(`/shop/${seller._id}`);
+    }
+  }, [isSeller, navigate]);
   return (
     <div>
 <ShopRegistration />
